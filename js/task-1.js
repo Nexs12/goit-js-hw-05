a9a912429f2c25c7237e4fd0c6f703e26a3005a1
Nexs12 <152890031+Nexs12@users.noreply.github.com>
2024-04-20 16:48:59 +0300

    123

--- js/task-1.js
+++ js/task-1.js
@@ -56,22 +56,73 @@
 
 
 
-const pizzaPalace = {
-  pizzas: ["Supercheese", "Smoked", "Four meats"],
-  checkPizza(pizzaName) {
-    return pizzas.includes(pizzaName);
-  },
-  order(pizzaName) {
-    const isPizzaAvailable = checkPizza(pizzaName);
+// const pizzaPalace = {
+//   pizzas: ["Supercheese", "Smoked", "Four meats"],
+//   checkPizza(pizzaName) {
+//     return pizzas.includes(pizzaName);
+//   },
+//   order(pizzaName) {
+//     const isPizzaAvailable = checkPizza(pizzaName);
 
-    if (!isPizzaAvailable) {
-      return `Sorry, there is no pizza named ${pizzaName}`;
-    }
+//     if (!isPizzaAvailable) {
+//       return `Sorry, there is no pizza named ${pizzaName}`;
+//     }
 
-    return `Order accepted, preparing ${pizzaName} pizza`;
-  },
-};
+//     return `Order accepted, preparing ${pizzaName} pizza`;
+//   },
+// };
 
-console.log(pizzaPalace.order("Smoked"));
-console.log(pizzaPalace.order("Four meats"));
-console.log(pizzaPalace.order("Smoked"));
+// console.log(pizzaPalace.order("Smoked"));
+// console.log(pizzaPalace.order("Four meats"));
+// console.log(pizzaPalace.order("Smoked"));
+
+class User {
+  email;
+
+  constructor(email) {
+    this.email = email;
+  }
+
+  get email() {
+    return this.email;
+  }
+
+  set email(newEmail) {
+    this.email = newEmail;
+  }
+}
+class Admin extends User {
+  blacklistedEmails = [];
+  
+static role = {
+    BASIC: "basic",
+    SUPERUSER: "superuser",
+  };
+
+  constructor(params) {
+    super(params.email);
+    this.access = params.access;
+  }
+
+    blacklist(email) {
+    this.blacklistedEmails.push(email);
+  }
+
+  isBlacklisted(email) {
+    return this.blacklistedEmails.includes(email);
+  }
+  
+}
+
+const mango = new Admin({
+  email: "mango@mail.com",
+  access: Admin.role.SUPERUSER,
+});
+
+console.log(mango.email); // "mango@mail.com"
+console.log(mango.access); // "superuser"
+
+mango.blacklist("poly@mail.com");
+console.log(mango.blacklistedEmails); // ["poly@mail.com"]
+console.log(mango.isBlacklisted("mango@mail.com")); // false
+console.log(mango.isBlacklisted("poly@mail.com")); // true
